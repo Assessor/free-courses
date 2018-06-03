def68a0616b4fe5f893004ece7f89a746eb26926
Ivan Maltsev <ivan.maltsev78@gmail.com>
2018-06-03 18:39:36 +0500

    Пробую заработать 10 баллов

---
+++ task1.js
@@ -0,0 +1,12 @@
+let str = '';
+process.argv.forEach((val, index) => {
+    if (index > 1) {
+        str += val.replace(/[^a-zA-Zа-яА-Я]/g, '').toUpperCase();
+    }    
+});
+
+if (str === str.split('').reverse().join('')) {
+    process.stdout.write('YES');
+} else {
+    process.stdout.write('NO');
+}
