7cea0221091594b6fde1687fd99f402b0cbe8bbe
Ivan Maltsev <ivan.maltsev78@gmail.com>
2018-06-06 09:49:39 +0500

    Убрал концевые пробелы и перевел на стрелочные функции

--- task3.js
+++ task3.js
@@ -10,9 +10,13 @@ const hours = Math.floor(((sec % 31536000) % 86400) / 3600);
 const minutes = Math.floor((((sec % 31536000) % 86400) % 3600) / 60);
 const seconds = (((sec % 31536000) % 86400) % 3600) % 60;
 
-function suffixNum (n, words) {
+const suffixNum = (n, words) => {
     return words[(n % 10 == 1 && n % 100 != 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2)];
 }
+
+const trim = (str) => {
+    return str.replace(/^\s+|\s+$/g, '');
+};
 
 let result = '';
 if (days > 0) {
@@ -31,4 +35,4 @@ if (seconds > 0) {
     result += seconds + suffixNum(seconds, [' секунда ', ' секунды ', ' секунд ']);
 }
 
-process.stdout.write(result);
+process.stdout.write(trim(result));
